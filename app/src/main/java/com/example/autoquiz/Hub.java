eeabc459043f7feff658e079f7b250fe36bc13ce
haykoo161 <haykoo161@gmail.com>
2024-03-17 13:54:27 +0400

    1st commit

---
+++ app/src/main/java/com/example/autoquiz/Hub.java
@@ -0,0 +1,21 @@
+package com.example.autoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Hub extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hub);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/autoquiz/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.autoquiz;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        },1000);
+    }
+}
