8b14a732fa3b05e83c3b5c2fbddaa715259287d8
haykoo161 <haykoo161@gmail.com>
2024-05-12 21:01:31 +0400

    50%)

---
+++ app/src/main/java/com/example/autoquiz/Game1.java
@@ -0,0 +1,18 @@
+package com.example.autoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import com.yourcompanyname.product.R
+public class Game1 extends AppCompatActivity {
+
+    private int[] bigHorse = {R.drawable.RR Wraith, }
+    private ImageView[] smallHorse = {R.drawable.name2}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game1);
+    }
+}
